521c303f0316f56ff7d42f3df61ff76fb919d423
Takson <tmap78@gmail.com>
2017-03-24 21:24:09 +0000

    df

--- login.php
+++ login.php
@@ -1,11 +1,38 @@
 <?php
-include("connection.php");// Establishing connection with our database
+include("login.php");// Establishing connection with our database
 /**
  * Created by PhpStorm.
  * User: 1611403
  * Date: 23/03/2017
  * Time: 14:48
  */
+
+ if(empty($_POST["username"]) || empty($_POST["password"]))
+ {
+     echo "Both fields are required.";
+ }else
+     $username = $_POST['username'];
+     $password = $_POST['password'];
+     $sql = "SELECT uid FROM users
+             WHERE username=' $username' and password='$password'";
+     $result = mysqli_query($db, $sql);
+     if (mysqli_num_rows($result) == 1) {
+         {
+             header("location:index.html");
+         }
+     }
+
+         // Redirecting To another Page
+     else
+         {
+             {
+             echo "Incorrect username or password.";
+         }
+
+}
+
+
+?>
 
 
 
