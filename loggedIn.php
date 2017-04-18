b4d9c03307c34f23d593b716d52fe58279df9079
Takson <tmap78@gmail.com>
2017-04-18 17:08:23 +0100

    df

--- loggedIn.php
+++ loggedIn.php
@@ -14,12 +14,15 @@ function displayAccesslevelInformation($accessLevel)
     if ($accessLevel == "student")
         echo "<p style = \"background-color:purple\">You are currently logged in as a student</p>";
 
-    elseif ($accessLevel == "root"){
+        elseif ($accessLevel == "root"){
         echo "<p<p style = \"background-color: red\">You are currently logged in as admin</p>";
          echo "<p<p style = \"background-color: red\">You now have access to additional admin features</p>";
 
     }
 }
-header('Location: index.php');
+header('Location: student_index.php');
+ob_start();
+require 'student\index.php';
+$output = ob_get_clean();
 
 ?>
