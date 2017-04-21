3736e2124b3bb7722ed1bfac5e3f8c3ca8ab2018
Takson <tmap78@gmail.com>
2017-04-21 19:25:17 +0100

    df

---
+++ classes/Db.php
@@ -0,0 +1,41 @@
+<?php
+
+class Db {
+
+    private $_connection;
+    private static $_instance;
+
+    /**
+     * 
+     * @return 
+     */
+    
+    public static function getInstance() {
+        if (!self::$_instance) {
+            self::$_instance = new self();
+        }
+
+        return self::$_instance;
+    }
+
+    public function __construct() {
+        // change the username and password to that set on your system {root - username} {dorespt: password}
+        $this->_connection = new mysqli("127.0.0.1", "root", "dore5pt", "college");
+
+        if (mysqli_connect_error()) {
+            trigger_error("Failed to connect to the database", E_USER_ERROR);
+        }
+    }
+
+    /**
+     * Returns a mysqli object
+     * @return mysqli_object
+     */
+    public function getConnection() {
+        return $this->_connection;
+    }
+
+    // TO PREVENT CLONING OF THE DATABASE OBJECT
+    public function __clone() {}
+
+}
