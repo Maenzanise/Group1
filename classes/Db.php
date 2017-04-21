7a1cf511c963fde7da58641ad5b38689026ba7db
Takson <tmap78@gmail.com>
2017-04-21 20:56:25 +0100

    updated password

--- classes/Db.php
+++ classes/Db.php
@@ -20,7 +20,7 @@ class Db {
 
     public function __construct() {
         // change the username and password to that set on your system {root - username} {dorespt: password}
-        $this->_connection = new mysqli("localhost", "root", "dore5pt", "college");
+        $this->_connection = new mysqli("localhost", "root", "Takson08", "college");
 
         if (mysqli_connect_error()) {
             trigger_error("Failed to connect to the database", E_USER_ERROR);
